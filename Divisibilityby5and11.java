2365ab763a78564613c9e762df323ce0612c4425
dharshana14 <dharshanakandhasamy>
2026-08-20 21:36:27 +0530

    Add divisibilityby5and11 program java program

---
+++ Divisibilityby5and11.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class Divisible5And11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if (n % 5 == 0 && n % 11 == 0)
+            System.out.println("Divisible");
+        else
+            System.out.println("Not Divisible");
+    }
+}
