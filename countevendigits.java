c55f4c140a2a1ba0240ab9be21a52deb26503b6f
dharshana14 <dharshanakandhasamy>
2026-08-21 19:21:58 +0530

    Add count even digits

---
+++ countevendigits.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class CountEvenDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), count = 0;
+
+        while (n > 0) {
+            if ((n % 10) % 2 == 0) count++;
+            n /= 10;
+        }
+
+        System.out.println(count);
+    }
+}
