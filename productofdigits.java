2d789a5601876b7a6be9011f95ef12da2672a667
dharshana14 <dharshanakandhasamy>
2026-08-21 19:15:26 +0530

    Add product of digits

---
+++ productofdigits.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class ProductDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), product = 1;
+
+        while (n > 0) {
+            product *= n % 10;
+            n /= 10;
+        }
+
+        System.out.println(product);
+    }
+}
