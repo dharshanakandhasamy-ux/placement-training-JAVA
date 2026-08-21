31e17b4c73d208219fd5e4211b54b94b32dd497b
dharshana14 <dharshanakandhasamy>
2026-08-21 10:35:02 +0530

    Add primenumber program

---
+++ primenumber.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        boolean prime = true;
+
+        if (n < 2)
+            prime = false;
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        System.out.println(prime ? "Prime" : "Not Prime");
+    }
+}
