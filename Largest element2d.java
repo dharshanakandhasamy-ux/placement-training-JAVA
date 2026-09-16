1431609a4bfa839bd9ae7727096ae2a9af39c96f
dharshana14 <dharshanakandhasamy>
2026-09-16 10:56:03 +0530

    largest 2D ARRAY

--- Largest element2d.java
+++ Largest element2d.java
@@ -5,16 +5,16 @@ class Main {
         Scanner sc = new Scanner(System.in);
 
         int[][] a = new int[3][3];
-        int sum = 0;
+        int max = Integer.MIN_VALUE;
 
         for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
+            for (int j = 0; j < 3; j++) {
                 a[i][j] = sc.nextInt();
 
-        for (int i = 0; i < 3; i++)
-            for (int j = 0; j < 3; j++)
-                sum += a[i][j];
+                if (a[i][j] > max)
+                    max = a[i][j];
+            }
 
-        System.out.println("Sum = " + sum);
+        System.out.println("Largest = " + max);
     }
 }
